bd95d47f91e1e7252a45259765f58e78e8ab3ed5
kevaf <kevinkipyegon20@gmail.com>
2021-09-09 16:35:20 +0300

    test changes

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY='f5eeacaa22f844141be14b60281cf8a8'
+    export MOVIE_API_KEY='f5eeacaa22f844141be14b60281cf8a8'
 export SECRET_KEY='os.urandom(32)'
 
 python3.8 manager.py server
