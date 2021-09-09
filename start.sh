35ba3c1de7dbcaea492d02d8c1d8d0637b72ebf8
kevaf <kevinkipyegon20@gmail.com>
2021-09-09 14:27:11 +0300

    mods secret key

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
- export MOVIE_API_KEY='f5eeacaa22f844141be14b60281cf8a8'
-export SECRET_KEY='kevaf'
+export MOVIE_API_KEY='f5eeacaa22f844141be14b60281cf8a8'
+# export SECRET_KEY='kevaf'
 
 python3.8 manage.py server
