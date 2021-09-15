03f5f0e610e1233f9171e3136df4e2c313baf251
kevaf <kevinkipyegon20@gmail.com>
2021-09-16 01:31:42 +0300

    added database url

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kev2214@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
 
 
