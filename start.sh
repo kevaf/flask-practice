ac99a90a4459252a8c2a3fd45142d5df177a4981
kevaf <kevinkipyegon20@gmail.com>
2021-09-16 23:38:55 +0300

    made changes

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MOVIE_API_KEY='f5eeacaa22f844141be14b60281cf8a8'
-export SECRET_KEY='os.urandom(32)'
+export SECRET_KEY='kevaf'
 export MAIL_USERNAME='kevohronoh@gmail.com'
 export MAIL_PASSWORD='alicerono2214'
 
