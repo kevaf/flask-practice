36bbcfeee53b67c80b43163be78d9988a122c422
kevaf <kevinkipyegon20@gmail.com>
2021-09-16 23:57:22 +0300

    mod secret key

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MOVIE_API_KEY='f5eeacaa22f844141be14b60281cf8a8'
-export SECRET_KEY='kevaf'
+export SECRET_KEY='kevafqqqq'
 export MAIL_USERNAME='kevohronoh@gmail.com'
 export MAIL_PASSWORD='alicerono2214'
 
